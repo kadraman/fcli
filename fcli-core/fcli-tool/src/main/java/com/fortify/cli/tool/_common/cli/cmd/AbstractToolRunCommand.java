6cd343f15ad4dddd7f35745e7f60daf277bf5d37
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2025-01-30 10:20:27 +0100

    chore: Add fcli run cmd, change stdout->debug log
    feat: New `fcli tool fcli run` command
    
    chore: Fix debug statement to use debug log instead of stdout

--- fcli-core/fcli-tool/src/main/java/com/fortify/cli/tool/_common/cli/cmd/AbstractToolRunCommand.java
+++ fcli-core/fcli-tool/src/main/java/com/fortify/cli/tool/_common/cli/cmd/AbstractToolRunCommand.java
@@ -73,7 +73,7 @@ public abstract class AbstractToolRunCommand extends AbstractRunnableCommand {
                 .filter(Objects::nonNull)
                 .flatMap(List::stream)
                 .toList();
-        System.out.println("Attempting to run "+fullCmd);
+        LOG.debug("Attempting to run "+fullCmd);
         var process = new ProcessBuilder()
                 .command(fullCmd)
                 .directory(new File(workDir))
